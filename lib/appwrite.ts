24bca24e8adf1c979c25e019b0a5c372f5ee5611
Thiago <teyextreme@outlook.com>
2024-07-11 16:29:44 -0300

    sing-up

---
+++ lib/appwrite.ts
@@ -0,0 +1,6 @@
+export const appwriteConfig = {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    platform: 'com.jsm.aoracourse',
+    projectid: '668fec53000ab747d856',
+    databaseId: '668ff066001d2ecd58dc'
+}
